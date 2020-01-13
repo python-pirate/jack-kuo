226268003395646449e49e06ac647bc950b30941
Jack Kuo <jackgwy@dingtalk.com>
2020-01-13 22:57:26 +0800

    selection sort
    Resolve #2
    
    Signed-off-by: Jack Kuo <jackgwy@dingtalk.com>

--- demo.py
+++ demo.py
@@ -1,2 +1,13 @@
 def select_sort(arr):
-    pass
+    j = len(arr)
+    while j > 1:
+        m = 0
+        i = 1
+
+        while i < j:
+            if arr[i] > arr[m]:
+                m = i
+            i += 1
+        if m != i - 1:
+            arr[m], arr[i - 1] = arr[i - 1], arr[m]
+        j -= 1
