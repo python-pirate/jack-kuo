46183f17625d63e2e51258d6f2b1d2efdf07bd63
Jack Kuo <jackgwy@dingtalk.com>
2020-01-13 17:55:54 +0800

    max/min value
    Resolve #1
    
    Signed-off-by: Jack Kuo <jackgwy@dingtalk.com>

--- test_max_min.py
+++ test_max_min.py
@@ -1,11 +1,13 @@
 from demo import *
+import random
+
 
 def test_max_min():
     arr = []
     for n in range(10):
         arr.append(random.randint(10, 100))
-    
+
     m = max_min(arr)
 
     for x in arr:
-        assert(max >= x)
+        assert(m >= x)
